68b8658635879a00ae79cb5599e666913472e2c2
Lefteris Giannaros <lefterisgiannaros@gmail.com>
2022-02-21 16:51:23 +0200

    dump

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD4HMS6c1H05A9d9jZecUZEFFhiIo6mnkQ",
+    authDomain: "challenge-29731.firebaseapp.com",
+    projectId: "challenge-29731",
+    storageBucket: "challenge-29731.appspot.com",
+    messagingSenderId: "283248746901",
+    appId: "1:283248746901:web:47325d28e58c59bc3f569c",
+    measurementId: "G-ZG7DQ9QC66"
+  };

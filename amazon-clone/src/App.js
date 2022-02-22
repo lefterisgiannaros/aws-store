9e87ddab3e64a6b4903bdbb98a858be27e8eb83c
Lefteris Giannaros <lefterisgiannaros@gmail.com>
2022-02-22 15:56:01 +0200

    punch it

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import Checkout from './Checkout';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/checkout" element={[<Header/> , <Checkout/>]}/>
+          <Route path="/" element={[<Header/> , <Home/>]}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
